fd72c2c87ba73b6025bb6452e8a5de94c3884a0a
SharifuzzamanShaon <shasan2200@gmail.com>
2024-09-25 17:57:25 +0600

    Revert "update user avatar"
    This reverts commit f2ec67ed9c9fff78c057f2f1c0c20fa691bdaa3d.

--- api/router/user.js
+++ api/router/user.js
@@ -1,9 +1,8 @@
-const { userUpdate, userDelete, updateAvatar } = require("../controller/user");
+const { userUpdate, userDelete } = require("../controller/user");
 const authMiddleware = require("../middleware/authenticate/authMiddleware");
 const router = require("express").Router();
 
 router.put("/update/:id", authMiddleware, userUpdate);
-router.patch("/upldate-avatar", authMiddleware, updateAvatar)
 router.delete("/delete/:id", authMiddleware, userDelete);
 
 module.exports = router;
--- client/my-app/next.config.mjs
+++ client/my-app/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['media.istockphoto.com','lh3.googleusercontent.com','res.cloudinary.com'], // Add the external image domain here
+        domains: ['media.istockphoto.com','lh3.googleusercontent.com'], // Add the external image domain here
       },
 };
 
