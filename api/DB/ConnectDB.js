98a4abdfbdd92ad26b81ee58f6103f3e3c6ecdc5
SharifuzzamanShaon <shasan2200@gmail.com>
2024-11-05 20:58:59 +0600

    db config

--- api/DB/ConnectDB.js
+++ api/DB/ConnectDB.js
@@ -1,7 +1,10 @@
 const { default: mongoose } = require("mongoose");
 
 exports.connectDB = async () => {
-    await mongoose.connect('mongodb://127.0.0.1:27017/LMS_tech');
-    // await mongoose.connect(process.env.DB_url);
-
-}
+  try {
+    await mongoose.connect(process.env.DB_CONNECTION_STR);
+  } catch (error) {
+    console.log("Db connecton error");
+  }
+  // await mongoose.connect(process.env.DB_url);
+};
